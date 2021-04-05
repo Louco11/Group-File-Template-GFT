e2345e1295c39faa5e87a3b21ba85cb0c2165bdd
Mikhail Dorontsov <MDorontsov@alfabank.ru>
2021-04-05 18:00:17 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.7.2"
+    java
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.louco"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.3.3"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
